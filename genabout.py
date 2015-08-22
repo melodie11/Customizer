a4380230cb574abd9f849bcf0eb8479c085fd4af
Ivailo Monev <xakepa10@gmail.com>
2015-08-22 22:45:31 +0300

    an experiment that aims to make the credits translatable

--- genabout.py
+++ genabout.py
@@ -3,9 +3,12 @@
 import sys, markdown
 
 save = False
+replace = None
 for arg in sys.argv:
     if arg == '--save':
         save = True
+    elif arg == '--replace':
+        replace = sys.argv[sys.argv.index(arg)+1]
 
 with open('Contributors', 'r') as f:
     converted = markdown.markdown(f.read())
@@ -13,5 +16,10 @@ with open('Contributors', 'r') as f:
 if save:
     with open('Contributors.html', 'w') as f:
         f.write(converted)
+elif replace:
+    with open(replace, 'r') as f:
+        content = f.read()
+    with open(replace.replace('.in', ''), 'w') as f:
+        f.write(content.replace('@CONTRIBUTORS@', converted))
 else:
     print(converted)
